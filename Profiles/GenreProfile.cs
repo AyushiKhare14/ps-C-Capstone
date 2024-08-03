1c341cf7626bc51dbefa4df95c1c81a7ca7470be
ayushi-khare-ps <167219286+ayushi-khare-ps@users.noreply.github.com>
2024-08-03 08:12:07 +0530

    Add files via upload

---
+++ Profiles/GenreProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using C_BookStoreBackEndAPI.Models;
+
+namespace C_BookStoreBackEndAPI.Profiles
+{
+    public class GenreProfile : Profile
+    {
+        public GenreProfile()
+        {
+            CreateMap<Models.Genre, Dtos.Genre.GenreDto>();
+            CreateMap<Dtos.Genre.CreateGenreDto, Models.Genre>();
+        }
+    }
+}

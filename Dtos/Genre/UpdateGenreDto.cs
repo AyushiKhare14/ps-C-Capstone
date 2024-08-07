97838dda4fc9ef995fba52c3fe4d65728ad6d703
ayushi-khare-ps <167219286+ayushi-khare-ps@users.noreply.github.com>
2024-08-07 18:35:43 +0530

    Add files via upload

--- Dtos/Genre/UpdateGenreDto.cs
+++ Dtos/Genre/UpdateGenreDto.cs
@@ -1,13 +1,9 @@
-﻿
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace C_BookStoreBackEndAPI.Dtos.Genre
 {
-    public class GenreDto
+    public class UpdateGenreDto
     {
-        public int Id { get; set; }
-
         [Required(ErrorMessage = "Genre Name is required.")]
         [MinLength(2, ErrorMessage = "Genre Name must be atleast 2 characters long.")]
         [MaxLength(20, ErrorMessage = "Genre Name must not exceed 20 characters.")]
